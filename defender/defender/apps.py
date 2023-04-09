a678e82abd730cd72a38e90470e3e83f8492b057
Sidharth Baveja <sidharthbav@gmail.com>
2023-04-08 21:51:56 -0500

    moving to a func

--- defender/defender/apps.py
+++ defender/defender/apps.py
@@ -11,6 +11,52 @@ import numpy as np
 # import pandas as pd
 import ember
 
+def experiment_with_removing_appended_data(app, pe, bytez):
+    # Determine the actual size of the file
+    actual_size = pe.OPTIONAL_HEADER.SizeOfHeaders
+    for section in pe.sections:
+        actual_size += section.SizeOfRawData
+
+    file_size = len(bytez)
+    #print('computed size', actual_size)
+    # print('bytez recieved size', file_size)
+
+    result, result_prob = None, None
+    try:
+        if actual_size != file_size:
+            #print('*************POTENITAL Manipulation')
+            #print('RUNNING WITH REAL SIZE')
+            custom_ext = CustomExtractor(bytez[:actual_size])
+
+            model = app.config['model']
+            result = 0
+            result_prob = custom_ext.custom_predict_with_threshold(model)
+            if result_prob[0][0]<0.61: result = 1
+
+            print('LABEL = ', result)
+            print('LABEL PROB = ', result_prob)
+        else:
+            custom_ext = CustomExtractor(bytez)
+            # attributes = custom_ext.custom_attribute_extractor()
+            # print(attributes['header'])
+
+
+            model = app.config['model']
+            # result = custom_ext.custom_predict_sample(model)
+            result_prob = custom_ext.custom_predict_with_threshold(model)
+            result = 0
+            if result_prob[0][0]<0.61: result = 1
+            # else: result = 0
+            # result = int(result)
+
+            print('LABEL = ', result)
+            print('LABEL PROB = ', result_prob)
+    except (lief.bad_format, lief.read_out_of_bound) as e:
+        print("Error:", e)
+        result = 1
+
+    return result, result_prob
+
 def create_app(model, threshold):
     app = Flask(__name__)
     app.config['model'] = model
@@ -32,49 +78,7 @@ def create_app(model, threshold):
             resp.status_code = 400  # Internal Server Error
             return resp
 
-
-        # Determine the actual size of the file
-        actual_size = pe.OPTIONAL_HEADER.SizeOfHeaders
-        for section in pe.sections:
-            actual_size += section.SizeOfRawData
-
-        file_size = len(bytez)
-        print('computed size', actual_size)
-        print('bytez recieved size', file_size)
-
-        try:
-            if actual_size != file_size:
-                print('*************POTENITAL Manipulation')
-                print('RUNNING WITH REAL SIZE')
-                custom_ext = CustomExtractor(bytez[:actual_size])
-
-                model = app.config['model']
-                result = 0
-                result_prob = custom_ext.custom_predict_with_threshold(model)
-                if result_prob[0][0]<0.61: result = 1
-
-                print('LABEL = ', result)
-                print('LABEL PROB = ', result_prob)
-            else:
-                custom_ext = CustomExtractor(bytez)
-                # attributes = custom_ext.custom_attribute_extractor()
-                # print(attributes['header'])
-
-
-                model = app.config['model']
-                # result = custom_ext.custom_predict_sample(model)
-                result_prob = custom_ext.custom_predict_with_threshold(model)
-                result = 0
-                if result_prob[0][0]<0.61: result = 1
-                # else: result = 0
-                # result = int(result)
-
-                print('LABEL = ', result)
-                print('LABEL PROB = ', result_prob)
-        except (lief.bad_format, lief.read_out_of_bound) as e:
-            print("Error:", e)
-            result = 1
-
+        result, result_prob = experiment_with_removing_appended_data(app, pe, bytez)
 
         if not isinstance(result, int) or result not in {0, 1}:
             resp = jsonify({'error': 'unexpected model result (not in [0,1])'})
