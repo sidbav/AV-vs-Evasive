f35156cd643e47ff0d686e665c9ea541209ac538
Sidharth Anil <sidharthanil98@gmail.com>
2023-04-09 17:58:26 -0500

    Created pe_to_npz file from the generate_long_long.py script

--- defender/pe_to_npz.py
+++ defender/pe_to_npz.py
@@ -23,7 +23,7 @@ import os
 
 
 def usage():
-  print("Usage: python generate_pickle_saved_array_of_data.py <DIRECTORY OF EITHER MALWARE OR GOODWARE FILES> -<b/m> <OUTPUT_FILE_NAME>")
+  print("Usage: python pe_to_npz.py <DIRECTORY OF EITHER MALWARE OR GOODWARE FILES> -<b/m> <OUTPUT_FILE_NAME>")
   print("Pass in either an either Malware folder OR entire Goodware Folder, since it will be easier to deal with")
   print()
   sys.exit()
@@ -44,6 +44,10 @@ if __name__ == "__main__":
   arr_of_features = []
 
 # gw1/0 gw2/1
+  X_train = []
+  y_train = []
+  print("Starting to go through the files")
+
   for path, subdirs, files in os.walk(directory_of_files):
     for name in files:
       file_name = os.path.join(path, name)
@@ -52,18 +56,26 @@ if __name__ == "__main__":
       with open(file_name, 'rb') as file:
         file_data = file.read()
         try:
-          file_data_features = PEAttributeExtractor(file_data).extract()
-          file_data_features.update({"label": label})
-          arr_of_features.append(file_data_features)
+          #file_data_features = PEAttributeExtractor(file_data).extract()
+          file_data_features = CustomExtractor(file_data).custom_feature_vector()
+          X_train.append(file_data_features)
+          y_train.append(label)
+          #file_data_features.update({"label": label})
+          
+          #arr_of_features.append(file_data_features)
         except Exception as inst:
           print(repr(inst))
           print("************************************An exception occurred")
 
-
+  print("Finished processing all the files")
 
 
   ##
-  fp = gzip.open(output_file,'wb')
-  cPickle.dump(arr_of_features,fp)
-  fp.close()
-
+#  fp = gzip.open(output_file,'wb')
+#  cPickle.dump(arr_of_features,fp)
+#  fp.close()
+  X_train = np.array(X_train)
+  y_train = np.array(y_train)
+  print("Shape of X_train = ", X_train.shape)
+  print("Shape of y_train = ", y_train.shape)
+  np.savez(output_file, x=X_train, y=y_train)
