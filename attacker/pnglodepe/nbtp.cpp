1d49354263d2781007c13c4469efef0d3a4f709a
sid38 <sidharthanil98@gmail.com>
2023-04-21 06:03:18 -0500

    nbtp works with directories instead of files

--- attacker/pnglodepe/nbtp.cpp
+++ attacker/pnglodepe/nbtp.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <tuple>
 #include <iterator>
+#include <Windows.h>
 
 typedef std::vector<unsigned char> DataVect;
 
@@ -42,24 +43,13 @@ DataVect decodeTwoSteps(const char* filename) {
     //if there's an error, display it
     if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-    // std::cout << "File bytes here: " << image[0] << image[1] << image[2] << image[3] << image[4] << image[5] << image[6] << image[8] << std::endl;
-
     return image;
-    //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 }
 
 DataVect get_file_bin(const std::string& filename){
     std::ifstream input( filename, std::ios::binary );
     DataVect buffer(std::istreambuf_iterator<char>(input), {});
     return buffer;
-
-    // std::ifstream file(filename);
-    // std::ostringstream ss;
-    // ss << file.rdbuf();
-    // file.close();
-    // const std::string& s = ss.str();
-    // DataVect vec(s.begin(), s.end());
-    // return vec;
 }
 
 void write_to_disk(const std::string& filename,const DataVect &data){
@@ -83,23 +73,46 @@ int main(int argc, char** argv){
     if (strcmp(argv[1],"b2p") == 0){
         
         // input binary data output png file
-        DataVect bin = get_file_bin(argv[2]);
-        int size = bin.size();
-        
-        // The 3 comes from the 24 bit RGB values. Each pixel uses 3 bytes of data
-        int area = ceil(float(size)/3.0);
+        std::string inputdir = argv[2];
+        std::string outputdir = argv[3];
+        WIN32_FIND_DATA fileData;
+        HANDLE hFind;
+        // std::cout << inputdir << outputdir << std::endl;
+        hFind = FindFirstFile((inputdir+"\\*.*").c_str(), &fileData);
+        if (hFind != INVALID_HANDLE_VALUE) {
+            do {
+                if (std::string(fileData.cFileName) != "." && std::string(fileData.cFileName) != ".."){
+                    DataVect bin = get_file_bin(inputdir + "\\" + fileData.cFileName);
+                    std::cout << inputdir + "\\" + fileData.cFileName << std::endl;
+                    int size = bin.size();
+                    
+                    // The 3 comes from the 24 bit RGB values. Each pixel uses 3 bytes of data
+                    int area = ceil(float(size)/3.0);
 
-        // Fixed width and calculating height accordingly
-        int width = 64;
-        int height = ceil(float(area)/float(width));
+                    // Fixed width and calculating height accordingly
+                    int width = 64;
+                    int height = ceil(float(area)/float(width));
 
-        int new_size = width*height*3;
-        std::cout << new_size << std::endl;
+                    int new_size = width*height*3;
+                    std::cout << new_size << std::endl;
 
-        // Appending additional data to fill out the image dimensions
-        bin.insert(bin.end(),new_size-size, '0');
+                    // Appending additional data to fill out the image dimensions
+                    bin.insert(bin.end(),new_size-size, '0');
+                    std::string output_filename = outputdir + "\\" + std::string(fileData.cFileName) + ".png";
+                    std::cout << output_filename << std::endl;
+                    encodeTwoSteps(output_filename.c_str(), bin, width, height);                    
+                
+                }
+            } while (FindNextFile(hFind, &fileData));
+            FindClose(hFind);
+        } else {
+            /* could not find any files in directory */
+            std::cerr << "Error: could not find any files in directory." << std::endl;
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
 
-        encodeTwoSteps(argv[3], bin, width, height);
+
     } else if (strcmp(argv[1],"p2b") == 0){
 
         DataVect binary_image = decodeTwoSteps(argv[2]);
